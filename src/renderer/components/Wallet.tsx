c48e8c3175b0234dcde926cd749ddc02b3fd6c40
Vadzim Mahilny <magilnyy.vadimo@gmail.com>
2023-09-05 11:44:59 +0100

    Transfer tokens

--- src/renderer/components/Wallet.tsx
+++ src/renderer/components/Wallet.tsx
@@ -1,14 +1,18 @@
-import { Box, Button, Heading, Layer, Text } from 'grommet';
+import { Box, Button, Heading, Layer, Text, TextInput } from 'grommet';
 import { useContext, useEffect, useState } from 'react';
 import { WalletContext } from 'renderer/context/walletContext';
 import truncateEthAddress from 'truncate-eth-address';
-import { useAccount, useConnect, useDisconnect } from 'wagmi';
+import { useAccount, useConnect, useDisconnect, useContractWrite, useWaitForTransaction } from 'wagmi';
 import { web3AuthInstance } from '../Web3AuthInstance';
 import { getPublicCompressed } from "@toruslabs/eccrypto";
+import { FLOCK_ABI, FLOCK_ADDRESS } from 'renderer/contracts/flock';
 
 function Wallet() {
   const { address } = useAccount();
   const [showWalletSettings, setShowWalletSettings] = useState(false);
+  const [transferAmount, setTransferAmount] = useState(0);
+  const [transferAddress, setTransferAddress] = useState('');
+  const [isTransferLoading, setIsTransferLoading] = useState(false);
   const { connectAsync, connectors } = useConnect();
   const { disconnect: wagmiDisconnect } = useDisconnect();
   const { nativeTokenBalance, flockTokenBalance } = useContext(WalletContext);
@@ -39,33 +43,65 @@ function Wallet() {
   }
 
   const loadUserInfo = async () => {
-      try {
-        const privateKey = await web3AuthInstance.provider?.request({
-          method: "eth_private_key", 
-        }) as string;
+    try {
+      const privateKey = await web3AuthInstance.provider?.request({
+        method: 'eth_private_key',
+      }) as string;
 
-        const publicKey = getPublicCompressed(Buffer.from(privateKey.padStart(64, "0"), "hex")).toString("hex");
-        const user = await web3AuthInstance.getUserInfo();
+      const publicKey = getPublicCompressed(
+        Buffer.from(privateKey.padStart(64, '0'), 'hex')
+      ).toString('hex');
+      const user = await web3AuthInstance.getUserInfo();
 
-        const res = await fetch("https://us-central1-flock-demo-design.cloudfunctions.net/postEmailToDB", {
-          method: "POST",
+      const res = await fetch(
+        'https://us-central1-flock-demo-design.cloudfunctions.net/postEmailToDB',
+        {
+          method: 'POST',
           headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + user.idToken, 
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${user.idToken}`,
           },
-          body: JSON.stringify({ pubKey: publicKey, email: user.email, wallet: address }),
-        });
+          body: JSON.stringify({
+            pubKey: publicKey,
+            email: user.email,
+            wallet: address,
+          }),
+        }
+      );
 
-        return res.json();
-      
-      } catch (error) {
-        console.error('Error loading user info:', error);
-      }
+      return res.json();
+    } catch (error) {
+      console.error('Error loading user info:', error);
+    }
+  };
+
+  const { data: dataTransfer, writeAsync: writeAsyncApprove } =
+    useContractWrite({
+      address: FLOCK_ADDRESS as `0x${string}`,
+      abi: FLOCK_ABI,
+      functionName: 'transfer',
+    });
+
+  const { isSuccess: isSuccessTransfer } = useWaitForTransaction({
+    hash: dataTransfer?.hash,
+  });
+
+  const handleTransfer = async () => {
+    setIsTransferLoading(true);
+    await writeAsyncApprove({
+      args: [transferAddress, transferAmount * 10 ** 18],
+    });
   };
 
   useEffect(() => {
+    if (isSuccessTransfer) {
+      setIsTransferLoading(false);
+    }
+  }, [isSuccessTransfer]);
+
+  useEffect(() => {
     if (address) {
-      loadUserInfo(); 
+      loadUserInfo();
     }
   }, [address]);
 
@@ -92,6 +128,38 @@ function Wallet() {
               <b>MATIC Balance: </b>
               {nativeTokenBalance ? nativeTokenBalance.formatted : 0} $MATIC
             </Text>
+            <Box gap="small" margin={{ top: 'small' }}>
+              <Text alignSelf="center" weight="bold">
+                Transfer FLC tokens
+              </Text>
+              <Box direction="row" gap="small">
+                <TextInput
+                  width="small"
+                  type="number"
+                  placeholder="Amount"
+                  value={transferAmount}
+                  onChange={(event: any) =>
+                    setTransferAmount(event.target.value)
+                  }
+                />
+                <TextInput
+                  width="medium"
+                  type="text"
+                  placeholder="Address"
+                  value={transferAddress}
+                  onChange={(event: any) =>
+                    setTransferAddress(event.target.value)
+                  }
+                />
+              </Box>
+
+              <Button
+                alignSelf='center'
+                label={isTransferLoading ? 'Transferring...' : 'Transfer'}
+                onClick={handleTransfer}
+                disabled={isTransferLoading}
+              />
+            </Box>
           </Box>
           <Box direction="row" alignSelf="end" gap="small">
             <Button
