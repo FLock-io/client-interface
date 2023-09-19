8ab0360c19c8d38220623f845a285ca949ed3687
Vadzim Mahilny <magilnyy.vadimo@gmail.com>
2023-09-19 19:19:38 +0100

    Disable external wallets

--- src/renderer/Web3AuthInstance.tsx
+++ src/renderer/Web3AuthInstance.tsx
@@ -24,7 +24,7 @@ export const web3AuthInstance = new Web3Auth({
   web3AuthNetwork: 'cyan',
   // @ts-ignore
   chainConfig,
-  authMode: 'DAPP',
+  authMode: 'WALLET',
   uiConfig: {
     theme: 'light',
     appName: 'FLock Client',
