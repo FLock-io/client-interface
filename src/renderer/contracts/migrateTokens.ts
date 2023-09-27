9bd35017c6ca7009ddc7422b7afa3d17fc5c6166
MIDAV0 <magilnyy.vadimo@gmail.com>
2023-09-27 11:14:23 +0100

    New contracts

---
+++ src/renderer/contracts/migrateTokens.ts
@@ -0,0 +1,87 @@
+export const MIGRATE_TOKENS_ADDRESS =
+  '0x1f808c9d65D992f822Ab1Fc33cc76Cda7334687c';
+
+export const MIGRATE_TOKENS_ABI = [
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '_oldTokenAddress',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: '_newTokenAddress',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'previousOwner',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'newOwner',
+        type: 'address',
+      },
+    ],
+    name: 'OwnershipTransferred',
+    type: 'event',
+  },
+  {
+    inputs: [],
+    name: 'migrate',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'owner',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'renounceOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'newOwner',
+        type: 'address',
+      },
+    ],
+    name: 'transferOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'withdraw',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+];
